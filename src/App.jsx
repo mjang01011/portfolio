6d33a2e0145fd1bf04bf2211137f2490bb96a0ac
Michael Jang <mjang01011@gmail.com>
2024-07-29 17:25:12 +0900

    [fix] Nested router issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
 import Skills from "./Components/Skills/Skills";
 import MyWork from "./Components/MyWork/MyWork";
-import Contact from "./Components/Contact/Contact";
+// import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <About />
-              <Skills />
-              <MyWork />
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+      <Hero />
+      <About />
+      <Skills />
+      <MyWork />
+    </>
   );
 };
 
