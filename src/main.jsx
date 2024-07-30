016eebb079aa1e106884d9025151a8e1c2507490
Michael Jang <mjang01011@gmail.com>
2024-07-30 20:51:02 +0900

    [edit] hero css

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>
 );
