c3d73a4e556fe4e5f103d9c58895b2811bcfcc16
Michael Jang <mjang01011@gmail.com>
2025-01-21 17:11:05 -0500

    [fix] BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -7,7 +7,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <BrowserRouter basename="/portfolio">
+  <HashRouter basename="/portfolio">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
